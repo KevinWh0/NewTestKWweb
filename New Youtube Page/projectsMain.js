b2dc8ae37e1eebedc6572891ed2d70230269a079
KevinWh0 <45321184+KevinWh0@users.noreply.github.com>
2019-10-28 18:20:40 -0700

    Add files via upload

--- New Youtube Page/projectsMain.js
+++ New Youtube Page/projectsMain.js
@@ -34,7 +34,6 @@ function menuToggle() {
         MainPos -= 1;
         elem.style.top = pos + "px";
         MainElems.style.top = MainPos + "%";
-        MainDesc.style.top = MainPos + 40 + "%";
         Body.style.backgroundSize = MainPos + 85 + "%";
       }
     } else {
@@ -45,47 +44,8 @@ function menuToggle() {
         MainPos += 1;
         elem.style.top = pos + "px";
         MainElems.style.top = MainPos + "%";
-        MainDesc.style.top = MainPos + 40 + "%";
         Body.style.backgroundSize = MainPos + 85 + "%";
       }
-    }
-  }
-}
-//!ANIMATION STAR STUFF STARTS
-function setProperty(duration, X, Y) {
-  var html = document.getElementsByTagName("html")[0];
-  //html.style.setProperty("--animation-delay", duration + "s");
-  html.style.setProperty("--zeroX", X + "px");
-  html.style.setProperty("--zeroY", -Y + "px");
-  html.style.setProperty("--DoneX", -X + "px");
-  html.style.setProperty("--DoneY", Y + "px");
-
-  //console.log(duration);
-  //changeAnimationTime();
-}
-//var start = 0;
-function changeAnimationTime() {
-  const animationDuration = Math.round(Math.random() * 50);
-  const X = Math.round(Math.random() * window.innerWidth);
-  const Y = Math.round(Math.random() * window.innerHeight);
-  //console.log(animationDuration);
-  setProperty(animationDuration, X, Y);
-}
-loopStar();
-function loopStar() {
-  var id = setInterval(loop, 70);
-
-  function loop() {
-    changeAnimationTime();
-  }
-}
-//!STAR ANIMATION STUFF ENDS
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if (new Date().getTime() - start > milliseconds) {
-      break;
     }
   }
 }
@@ -100,6 +60,8 @@ function RedirectAnimate() {
   var MainElems = document.getElementById("__Main");
   var MainDesc = document.getElementById("desc_Main");
 
+  var Body = document.getElementById("Body");
+
   var id = setInterval(frameR, 80);
 
   function frameR() {
@@ -109,6 +71,20 @@ function RedirectAnimate() {
   }
 }
 //TODO add animations/loading screens
+function redirectHome() {
+  //RedirectAnimate();
+  document.body.innerHTML = "";
+  //document.body.style.background = "#000000";
+  var btn = document.createElement("div");
+  btn.id = "Loading";
+  btn.innerHTML = "Loading";
+  window.location.replace("/Home.html");
+
+  document.body.appendChild(btn);
+
+  //Transition
+  //sleep(1000);
+}
 function redirectVids() {
   //RedirectAnimate();
   document.body.innerHTML = "";
@@ -116,20 +92,10 @@ function redirectVids() {
   var btn = document.createElement("div");
   btn.id = "Loading";
   btn.innerHTML = "Loading";
-
   window.location.replace("/Videos.html");
 
   document.body.appendChild(btn);
 
   //Transition
   //sleep(1000);
-}
-function redirectProjects() {
-  document.body.innerHTML = "";
-  //document.body.style.background = "#000000";
-  var btn = document.createElement("div");
-  btn.id = "Loading";
-  btn.innerHTML = "Loading";
-  window.location.replace("/Projects.html");
-  document.body.appendChild(btn);
 }
